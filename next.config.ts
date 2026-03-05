169ea8202cbea662e54b06af08a77685bdc21a31
Jose Rodriguez <jdamian.rodriguez@gmail.com>
2026-03-05 23:13:14 +0100

    remove invalid next config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: true
-  },
-  experimental: {
-    missingSuspenseWithCSRBailout: false
-  }
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
