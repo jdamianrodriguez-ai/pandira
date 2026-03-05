a4d422177788d15badc3c0a32c970ad3b69c8326
Jose Rodriguez <jdamian.rodriguez@gmail.com>
2026-03-05 21:29:10 +0100

    fix not-found prerender

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+export const dynamic = "force-dynamic"
+
+export default function NotFound() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>404</h1>
+      <p>Página no encontrada</p>
+    </div>
+  )
+}
