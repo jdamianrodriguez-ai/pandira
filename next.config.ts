4d708573ca4d2d4da1d04a86874368eb174e8638
Jose Rodriguez <jdamian.rodriguez@gmail.com>
2026-03-07 00:39:47 +0100

    fix next.config.ts2

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,14 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "media.rawg.io",
       },
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+      },
+      {
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+      },
     ],
   },
 };
