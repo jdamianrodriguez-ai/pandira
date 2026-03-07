bd0ad2c5f8a0c8d0f1e4e1ca1b72d54c1b1d15ee
Jose Rodriguez <jdamian.rodriguez@gmail.com>
2026-03-07 12:58:41 +0100

    Update login paths

---
+++ middleware.ts
@@ -0,0 +1,76 @@
+import { createServerClient } from "@supabase/ssr"
+import { NextResponse, type NextRequest } from "next/server"
+
+export async function middleware(request: NextRequest) {
+
+  let response = NextResponse.next()
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name) {
+          return request.cookies.get(name)?.value
+        },
+        set(name, value, options) {
+          request.cookies.set({
+            name,
+            value,
+            ...options,
+          })
+          response = NextResponse.next({
+            request,
+          })
+          response.cookies.set({
+            name,
+            value,
+            ...options,
+          })
+        },
+        remove(name, options) {
+          request.cookies.set({
+            name,
+            value: "",
+            ...options,
+          })
+          response = NextResponse.next({
+            request,
+          })
+          response.cookies.set({
+            name,
+            value: "",
+            ...options,
+          })
+        },
+      },
+    }
+  )
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  const { pathname } = request.nextUrl
+
+  const isLogin = pathname === "/login"
+
+  if (!user && !isLogin) {
+    return NextResponse.redirect(new URL("/login", request.url))
+  }
+
+  if (user && isLogin) {
+    return NextResponse.redirect(new URL("/movie", request.url))
+  }
+
+  return response
+}
+
+export const config = {
+  matcher: [
+    "/",
+    "/movie",
+    "/games",
+    "/collections/:path*"
+  ],
+}
