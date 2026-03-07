b18c2fbb42a58c72c18e3486133bf7114090ba1f
Jose Rodriguez <jdamian.rodriguez@gmail.com>
2026-03-07 12:34:37 +0100

    Update login page10

--- app/page.tsx
+++ app/page.tsx
@@ -15,30 +15,30 @@ export default async function Home() {
   }
 
   return (
-    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+    <div className="flex items-center justify-center h-[80vh] text-white">
 
       <div className="text-center space-y-10">
 
-        <h1 className="text-5xl font-bold">
+        <h1 className="text-6xl font-bold tracking-tight">
           Pandira
         </h1>
 
-        <p className="text-gray-400">
+        <p className="text-gray-400 text-lg">
           Tu colección digital
         </p>
 
-        <div className="flex gap-8 justify-center">
+        <div className="flex gap-6 justify-center">
 
           <Link
             href="/movie"
-            className="bg-white/10 hover:bg-white/20 transition px-8 py-4 rounded-xl"
+            className="bg-white/10 hover:bg-white/20 transition px-8 py-4 rounded-xl text-lg"
           >
             🎬 Películas
           </Link>
 
           <Link
             href="/games"
-            className="bg-white/10 hover:bg-white/20 transition px-8 py-4 rounded-xl"
+            className="bg-white/10 hover:bg-white/20 transition px-8 py-4 rounded-xl text-lg"
           >
             🎮 Videojuegos
           </Link>
