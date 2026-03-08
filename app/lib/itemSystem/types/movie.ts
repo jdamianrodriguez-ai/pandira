fcf1aaa1e99e21c5b18579a1ef28d8a82183ba42
Jose Rodriguez <jdamian.rodriguez@gmail.com>
2026-03-08 01:21:52 +0100

    tipologias

--- app/lib/itemSystem/types/movie.ts
+++ app/lib/itemSystem/types/movie.ts
@@ -1,3 +1,33 @@
+export interface Movie {
+  id: string
+  tmdb_id: number
+
+  title?: string
+  original_title?: string
+  year?: number
+
+  poster?: string | null
+  cover_url?: string | null
+
+  genre?: string[] | string
+  description?: string
+
+  runtime?: number
+  vote_average?: number
+
+  director?: string
+  actors?: string[] | string
+
+  format?: "DVD" | "Blu-ray"
+
+  status?: string
+  location?: string
+  value_estimate?: number
+
+  watched?: boolean
+  rating?: number
+}
+
 export const movieType = {
   type: "movie",
   label: "Movie",
